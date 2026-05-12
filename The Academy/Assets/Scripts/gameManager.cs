f7090ea4c0e877f8a86a0f73fd8559381e85ded6
rbdr4 <raymondor2006@gmail.com>
2026-05-12 16:08:34 -0400

    changed playerflash color

--- The Academy/Assets/Scripts/gameManager.cs
+++ The Academy/Assets/Scripts/gameManager.cs
@@ -60,8 +60,6 @@ public class gameManager : MonoBehaviour
     {
         isPaused = false;
         Time.timeScale = timeScaleOrig;
-        //Cursor.visible = false;
-        //Cursor.lockState = CursorLockMode.Locked;
         menuActive.SetActive(false);
         menuActive = null;
     }
