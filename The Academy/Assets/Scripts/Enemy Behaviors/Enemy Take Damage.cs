ebe6e2add49a048a1311a0ad799ed82ca649a257
Kasey Baker <klbaker2@student.fullsail.edu>
2026-05-14 14:48:09 -0400

    Enemies Flash Red on Hit

--- The Academy/Assets/Scripts/Enemy Behaviors/Enemy Take Damage.cs
+++ The Academy/Assets/Scripts/Enemy Behaviors/Enemy Take Damage.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
-
+using System.Collections;
+using UnityEngine.AI;
+using System.Linq;
 
 //HOW TO USE:
 /*
@@ -13,9 +15,23 @@ public class EnemyTakeDamage : MonoBehaviour, IDamage
 
     [SerializeField] float currHealth;
     [SerializeField] GameObject key;
+    [SerializeField] Renderer rend;
+
+    Color colorOrig;
+
+    Renderer[] allRenders;
+    Color[] allColors;
     void Start()
     {
+        colorOrig = rend.material.color;
         gameManager.instance.updateGameGoal(1);
+        allRenders = GetComponentsInChildren<Renderer>();
+        allColors = new Color[allRenders.Length];
+        for (int i = 0; i < allRenders.Length; i++)
+            {
+                allColors[i] = (allRenders[i].material.color);
+            }
+        
     }
 
     // Update is called once per frame
@@ -26,6 +42,8 @@ public class EnemyTakeDamage : MonoBehaviour, IDamage
 
     public void takeDamage(int amount)
     {
+
+        
         currHealth -= amount;
         if(currHealth <= 0)
         {
@@ -36,5 +54,25 @@ public class EnemyTakeDamage : MonoBehaviour, IDamage
             }
             Destroy(gameObject);
         }
+        else
+        {
+            StartCoroutine(flashRed());
+        }
+    }
+
+    IEnumerator flashRed()
+    {
+        rend.material.color = Color.red;
+        for(int i = 0; i < allRenders.Length; i++)
+        {
+            allRenders[i].material.color = Color.red;
+        }
+        yield return new WaitForSeconds(0.1f);
+        rend.material.color = colorOrig;
+        for(int i = 0; i < allRenders.Length; i++)
+        {
+            allRenders[i].material.color = allColors[i];
+        }
+        
     }
 }
