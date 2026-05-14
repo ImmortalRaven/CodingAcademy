689e8d3cd911b76cc5b19d88fd72572b4a4a76c0
ImmortalRaven <heydebrayden@gmail.com>
2026-05-14 15:45:42 -0400

    fixed trigger maybe on fear

--- The Academy/Assets/Scripts/Environment/Traps/Fear.cs
+++ The Academy/Assets/Scripts/Environment/Traps/Fear.cs
@@ -13,7 +13,7 @@ public class Fear : MonoBehaviour
     {
         
     }
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         if (other.gameObject == gameManager.instance.player)
         {
