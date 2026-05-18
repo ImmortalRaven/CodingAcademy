c573b5c823b6a1eba88b6e52188b7ba252a7e122
ImmortalRaven <heydebrayden@gmail.com>
2026-05-18 14:50:37 -0400

    bombs now properly implemented

---
+++ The Academy/Assets/Scripts/Environment/Traps/Bomb.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using System.Collections;
+
+public class Bomb : MonoBehaviour
+{
+    [SerializeField] float radius;
+    [SerializeField] int secToExplosion;
+    [SerializeField] Renderer rend;
+    [SerializeField] GameObject warningCircle;
+    Color colorOrig;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        colorOrig = rend.material.color;
+        warningCircle.transform.localScale += new Vector3(radius * 2, 0, radius * 2);
+        
+        StartCoroutine(explosion());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator explosion()
+    {
+        yield return StartCoroutine(bombFlash());
+
+        Collider[] explosion = Physics.OverlapSphere(transform.position, radius);
+
+        foreach (Collider obj in explosion)
+        {
+            IDamage dmg = obj.gameObject.GetComponent<IDamage>();
+            if (dmg != null)
+            {
+                dmg.takeDamage(5);
+            }
+            else if (obj.CompareTag("Breakable"))
+            {
+                Destroy(obj.gameObject);
+            }
+        }
+        Destroy(gameObject);
+    }
+
+    IEnumerator bombFlash()
+    {
+        for (int i = 0; i < secToExplosion; i++)
+        {
+            rend.material.color = Color.yellow;
+            yield return new WaitForSeconds(0.1f);
+            rend.material.color = colorOrig;
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
